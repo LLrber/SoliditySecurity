ccf06e3055a713a3592e7af723160f7df6179342
LLrber <LLrber@163.com>
2023-02-13 15:29:02 +0800

    mysql setting

--- SoliditySecurity/urls.py
+++ SoliditySecurity/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from detection import views
+from django.conf.urls import include
 
 urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^detection/', include('detection.urls')),
     path("admin/", admin.site.urls),
+
 ]
---
+++ detection/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# python manage.py makemigrations xxx
+# python manage.py migrate
+
